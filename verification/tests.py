174388c2fbf110bc006e0e5e26490bbc1e9a0fe6
Cameron Mims <cameronmims@hotmail.com>
2013-12-12 01:21:38 -0430

    Update tests.py

--- verification/tests.py
+++ verification/tests.py
@@ -17,12 +17,12 @@ TESTS = {
             "answer": 40,
             "explanation": ""
         },
-        {"input": [ [('1', 'C'), ('4', 'H')] ], "answer": 15 , "explanation": ""},
-        {"input": [ [('2', 'S'), ('10', 'D')] ], "answer": 22 , "explanation": ""},
-        {"input": [ [('3', 'C'), ('3', 'C')] ], "answer": 31 , "explanation": ""},
-        {"input": [ [('1', 'C'), ('1', 'H'), ('1', 'D'), ('1', 'S')] ], "answer": 4 , "explanation": ""},
-        {"input": [ [('A', 'C'), ('A', 'H'), ('A', 'D'), ('A', 'S')] ], "answer": 44 , "explanation": ""},
-        {"input": [ [('1', 'A'), ('2', 'A'), ('3', 'A'), ('4', 'A'), ('5', 'A'), ('6', 'A')] ], "answer": 46 , "explanation": ""},
+        {"input": [ ['1C', '4H'] ], "answer": 15 , "explanation": ""},
+        {"input": [ ['2S', '10D'] ], "answer": 22 , "explanation": ""},
+        {"input": [ ['3C', '3C'] ], "answer": 31 , "explanation": ""},
+        {"input": [ ['1C', '1H', '1D', '1S'] ], "answer": 4 , "explanation": ""},
+        {"input": [ ['AC', 'AH', 'AD', 'AS'] ], "answer": 44 , "explanation": ""},
+        {"input": [ ['1A', '2A', '3A', '4A', '5A', '6A'] ], "answer": 46 , "explanation": ""},
  
     ]
 }
